3e042a91e3556bc99ad5904b485fa4dae2e0151e
JeffRebiro <jeffrebiro@gmail.com>
2025-11-21 11:14:21 +0300

    Updating to v3

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -1,8 +1,16 @@
-import { Button, Flex, Heading, VStack, Box, useColorModeValue } from "@chakra-ui/react";
-import * as RadioGroup from "@radix-ui/react-radio-group"; // Radix UI
+import {
+  Button,
+  Flex,
+  Heading,
+  VStack,
+  Box,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useTheme as useNextTheme } from "next-themes";
 
 import { savePaymentMethod } from "../actions/cartActions";
 import CheckoutSteps from "../components/CheckoutSteps";
@@ -11,6 +19,8 @@ import FormContainer from "../components/FormContainer";
 const PaymentScreen = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { theme } = useNextTheme();
+  const isLight = theme === "light";
 
   const cart = useSelector((state) => state.cart);
   const { shippingAddress, paymentMethod } = cart;
@@ -37,39 +47,29 @@ const PaymentScreen = () => {
     <Flex w="full" alignItems="center" justifyContent="center" py="10">
       <FormContainer>
         <Box
-          bg={useColorModeValue("white", "gray.700")}
+          bg={isLight ? "white" : "gray.700"}
           boxShadow="lg"
           p="8"
           rounded="lg"
         >
-          {/* Checkout Steps */}
           <CheckoutSteps step1 step2 step3 />
 
-          {/* Heading */}
           <Heading as="h2" mb="6" fontSize="2xl" textAlign="center">
             Select Payment Method
           </Heading>
 
-          {/* Form */}
           <form onSubmit={submitHandler}>
             <VStack spacing="6" align="stretch">
-              <RadioGroup.Root
-                defaultValue={paymentMethodRadio}
-                onValueChange={setPaymentMethodRadio}
+              <RadioGroup
+                value={paymentMethodRadio}
+                onChange={setPaymentMethodRadio}
               >
                 <VStack align="start" spacing="4">
-                  <RadioGroup.Item value="paypal">
-                    <RadioGroup.ItemHiddenInput />
-                    <RadioGroup.ItemIndicator />
-                    <RadioGroup.ItemText>
-                      PayPal or Credit/Debit Card
-                    </RadioGroup.ItemText>
-                  </RadioGroup.Item>
-                  {/* Add more RadioGroup.Item elements if needed */}
+                  <Radio value="paypal">PayPal or Credit/Debit Card</Radio>
+                  {/* Add more payment options here if needed */}
                 </VStack>
-              </RadioGroup.Root>
+              </RadioGroup>
 
-              {/* Submit Button */}
               <Button
                 type="submit"
                 colorScheme="teal"
