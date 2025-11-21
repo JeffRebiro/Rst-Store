e75657a52f9ec91f7c2385bae2b33acb38ed3d2a
JeffRebiro <jeffrebiro@gmail.com>
2025-11-21 09:56:51 +0300

    Updating to v3

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,11 +1,14 @@
-import { Alert, AlertIcon, AlertTitle } from "@chakra-ui/react";
+import { Alert } from "@chakra-ui/react";
 
 const Message = ({ type = "info", children }) => {
   return (
-    <Alert>
-      <AlertIcon status={type} />
-      <AlertTitle>{children}</AlertTitle>
-    </Alert>
+    <Alert.Root status={type}>
+      <Alert.Indicator />
+      <Alert.Content>
+        <Alert.Title>{children}</Alert.Title>
+      </Alert.Content>
+    </Alert.Root>
   );
 };
+
 export default Message;
