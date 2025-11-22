11b9dcbfd70ce2c34ffad285b99ff5cbdc3cac80
JeffRebiro <jeffrebiro@gmail.com>
2025-11-22 07:22:11 +0300

    Updating to v3

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,13 +1,23 @@
-import { Alert } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
 const Message = ({ type = "info", children }) => {
+  const bgColor = type === "error" ? "red.100" : "blue.100";
+  const borderColor = type === "error" ? "red.300" : "blue.300";
+  const textColor = type === "error" ? "red.800" : "blue.800";
+
   return (
-    <Alert.Root status={type}>
-      <Alert.Indicator />
-      <Alert.Content>
-        <Alert.Title>{children}</Alert.Title>
-      </Alert.Content>
-    </Alert.Root>
+    <Box
+      bg={bgColor}
+      border="1px"
+      borderColor={borderColor}
+      color={textColor}
+      px="4"
+      py="3"
+      borderRadius="md"
+      mb="4"
+    >
+      {children}
+    </Box>
   );
 };
 
