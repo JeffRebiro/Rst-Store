6437fd6382eaf6dd36aa31ab42582618c1d17efc
JeffRebiro <jeffrebiro@gmail.com>
2025-11-22 08:07:22 +0300

    Updating to v3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
 import { Flex } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import ShippingScreen from "./screens/ShippingScreen";
-import PaymentScreen from "./screens/PaymentScreen";
-import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import OrderScreen from "./screens/OrderScreen";
-import OrderListScreen from "./screens/OrderListScreen";
-import UserEditScreen from "./screens/UserEditScreen";
-import UserListScreen from "./screens/UserListScreen";
-import ProductListScreen from "./screens/ProductListScreen";
-import ProductEditScreen from "./screens/ProductEditScreen";
+import Footer from "./components/Footer";
+import TestScreen from "./screens/TestScreen";
 
 function App() {
   return (
@@ -32,22 +18,9 @@ function App() {
         bgColor="white"
       >
         <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/products/:id" element={<ProductScreen />} />
-          <Route path="/cart" element={<CartScreen />} />
-          <Route path="/cart/:id" element={<CartScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/profile" element={<ProfileScreen />} />
-          <Route path="/shipping" element={<ShippingScreen />} />
-          <Route path="/payment" element={<PaymentScreen />} />
-          <Route path="/placeorder" element={<PlaceOrderScreen />} />
-          <Route path="/order/:id" element={<OrderScreen />} />
-          <Route path="/admin/userlist" element={<UserListScreen />} />
-          <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
-          <Route path="/admin/productlist" element={<ProductListScreen />} />
-          <Route path="/admin/product/:id/edit" element={<ProductEditScreen />} />
-          <Route path="/admin/orderList" element={<OrderListScreen />} />
+          <Route path="/" element={<TestScreen />} />
+          <Route path="/test" element={<TestScreen />} />
+          {/* Comment out all other routes */}
         </Routes>
       </Flex>
       <Footer />
