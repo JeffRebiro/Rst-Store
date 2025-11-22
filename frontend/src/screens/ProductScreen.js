966b3a2c7c813762d3a5422928fe683082711da7
JeffRebiro <jeffrebiro@gmail.com>
2025-11-22 07:01:03 +0300

    Updating to v3

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -2,7 +2,7 @@ import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
 import Loader from "../components/Loader";
-import Message from "../components/Message";
+import Rating from "../components/Rating";
 
 const ProductScreen = () => {
   const { id } = useParams();
@@ -15,8 +15,8 @@ const ProductScreen = () => {
       <Box>
         <Text>Product ID: {id}</Text>
         <Loader />
-        <Message type="info">This is an info message</Message>
-        <Message type="error">This is an error message</Message>
+        <Rating value={3.5} text="123 reviews" />
+        <Rating value={4} text="456 reviews" color="blue.500" />
       </Box>
     </>
   );
