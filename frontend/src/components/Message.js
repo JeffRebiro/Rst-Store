479d9dc587c2911ce96755175f205641bbe815d1
JeffRebiro <jeffrebiro@gmail.com>
2025-11-22 07:08:52 +0300

    Updating to v3

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -2,9 +2,12 @@ import { Alert } from "@chakra-ui/react";
 
 const Message = ({ type = "info", children }) => {
   return (
-    <Alert status={type}>
-      {children}
-    </Alert>
+    <Alert.Root status={type}>
+      <Alert.Indicator />
+      <Alert.Content>
+        <Alert.Title>{children}</Alert.Title>
+      </Alert.Content>
+    </Alert.Root>
   );
 };
 
